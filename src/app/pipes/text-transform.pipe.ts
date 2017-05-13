4c63c2829440f65acecdd0b121b1f36dcd36ee9d
Edward Payton <edwardjpayton@me.com>
2017-05-13 18:56:12 +0100

    Removed console logs

--- src/app/pipes/text-transform.pipe.ts
+++ src/app/pipes/text-transform.pipe.ts
@@ -14,7 +14,6 @@ export class FirstUppercasePipe implements PipeTransform {
         .join(' ');
     }
     else {
-        console.log('f - not',string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase());
         return string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
     }
     
